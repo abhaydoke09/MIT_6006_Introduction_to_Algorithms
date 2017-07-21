b8b57427ff76a23e61498b709573021d3283ed53
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-20 21:39:41 -0400

    added functionality to slice the terrain

--- 2DPeak.py
+++ 2DPeak.py
@@ -19,6 +19,18 @@ class 2DPeak(object):
 				}
 		return options[direction]
 
+	def sliceTheTerrain(self, direction, middle_row, middle_col):
+		options = { 1: self.terrain[:, :middle_col-1],
+					2: self.terrain[:middle_row-1, :middle_col-1],
+					3: self.terrain[:middle_row-1, :],
+					4: self.terrain[:middle_row-1, middle_col+1:],
+					5: self.terrain[:, middle_col+1:],
+					6: self.terrain[middle_row+1:, middle_col+1:],
+					7: self.terrain[middle_row+1:, :],
+					8: self.terrain[middle_row+1:, :middle_col-1],
+				}
+		self.terrain = options[direction]
+		return
 
 	def findPeak(self):
 		if getTerrainSize<=4:
@@ -29,28 +41,30 @@ class 2DPeak(object):
 		if middle_row==0 and middle_col==0:
 			neighbors = {a: getElement(middle_row, middle_column, a) for a in [5,6,7]}
 
+
 		elif middle_row==0 and middle_col==len(self.terrain[0][0])-1:
-			1,8,7
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,8,7]}
 
 		elif middle_row==len(self.terrain[0])-1 and middle_col==0:
-			3,4,5
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [3,4,5]}
 
 		elif middle_row==len(self.terrain[0])-1 and middle_col==len(self.terrain[0][0])-1:
-			1,2,3
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [8,6,7]}
 
-		elif middle_row==0:	8,6,7
+		elif middle_row==0:	
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,8,7]}
 
 		elif middle_row==len(self.terrain[0])-1:
-			2,3,4
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [2,3,4]}
 
 		elif middle_col==0:
-			4,5,6
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [4,5,6]}
 
 		elif middle_col==len(self.terrain[0][0])-1:
-			2,1,8
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [2,1,8]}
 
 		else:
-			1,2,3,4,5,6,7,8
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,2,3,4,5,6,7,8]}
 
 
 
