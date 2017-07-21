419162143cb19fd69f4fa8ec4007cec34bc46c02
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-20 20:49:56 -0400

    added 1D_peak

---
+++ 1D_peak.py
@@ -0,0 +1,20 @@
+s = [1,2,3,4,4,5,6,3,2,1,3]
+
+def find_peak(a, start, end):
+	middle = (start+end)/2
+	if a[middle-1]<=a[middle] and a[middle+1]<=a[middle]:
+		print a[middle]
+		return
+	if start==end:
+		print a[start]
+		return
+	if end==start+1:
+		print max(a[start], a[end])
+		return
+	else:
+		if a[middle]>a[middle-1] and a[middle]<=a[middle+1]:
+			find_peak(a, middle+1, end)
+		else:
+			find_peak(a, start, middle-1)
+
+find_peak(s,0,len(s)-1)
