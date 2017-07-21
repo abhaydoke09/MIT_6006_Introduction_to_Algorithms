c3a352e9b356e9de14f413f0a7ca9837ce866c56
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-20 22:07:04 -0400

    added function to display terrain

--- 2D_Peak.py
+++ 2D_Peak.py
@@ -6,7 +6,7 @@ class TwoDPeak(object):
 		self.terrain = np.array(terrain)
 
 	def getTerrainSize(self):
-		return len(self.terrain[0])*len(self.terrain[0][0])
+		return self.terrain.shape[0]*self.terrain.shape[1]
 
 	def getElement(self, middle_row, middle_col, direction):
 		options = { 1: self.terrain[middle_row, middle_col-1],
@@ -33,85 +33,93 @@ class TwoDPeak(object):
 		self.terrain = options[direction]
 		return
 
+	def displayTerrain(self):
+		for i in range(self.terrain.shape[0]):
+			for j in range(self.terrain.shape[1]):
+				print self.terrain[i,j]," "
+			print ""
+		print "##########"
+		
 	def findPeak(self):
 		if self.getTerrainSize<=4:
 			return 
+		self.displayTerrain()
 		middle_row = (self.terrain.shape[0])/2
 		middle_col = (self.terrain.shape[1])/2
 
-		if self.getTerrainSize<=4:
+		if self.getTerrainSize()<=4:
 			print "peak ==> ", max(self.terrain)
 
 
 		if middle_row==0 and middle_col==0:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [5,6,7]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [5,6,7]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 
 		elif middle_row==0 and middle_col==self.terrain.shape[1]-1:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,8,7]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [1,8,7]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_row==self.terrain.shape[0]-1 and middle_col==0:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [3,4,5]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [3,4,5]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_row==self.terrain.shape[0]-1 and middle_col==self.terrain.shape[1]-1:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [8,6,7]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [8,6,7]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_row==0:	
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,8,7]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [1,8,7]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_row==self.terrain.shape[0]-1:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [2,3,4]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [2,3,4]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_col==0:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [4,5,6]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [4,5,6]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		elif middle_col==self.terrain.shape[1]-1:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [2,1,8]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [2,1,8]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
 
 		else:
-			neighbors = {a: getElement(middle_row, middle_column, a) for a in [1,2,3,4,5,6,7,8]}
-			max_val = max(neighbors.values)
-			if terrain[middle_row,middle_col]>max_val:
+			neighbors = {a: self.getElement(middle_row, middle_col, a) for a in [1,2,3,4,5,6,7,8]}
+			max_val = max(neighbors.values())
+			if self.terrain[middle_row,middle_col]>max_val:
 				print "peak ==> ", self.terrain[middle_row, middle_col]
 			self.sliceTheTerrain(middle_row, middle_col, max(neighbors, key=lambda key: neighbors[key]))
 			self.findPeak()
