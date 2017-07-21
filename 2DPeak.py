3c43bff3d2fcb007bf4055a661b057b5a0c48e0e
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-20 21:31:24 -0400

    get neighboring element added

--- 2DPeak.py
+++ 2DPeak.py
@@ -1,10 +1,24 @@
 import numpy as np
+
 class 2DPeak(object):
 	def __init__(self, terrain):
 		self.terrain = np.array(terrain)
 
 	def getTerrainSize(self):
 		return len(self.terrain[0])*len(self.terrain[0][0])
+
+	def getElement(self, middle_row, middle_col, direction):
+		options = { 1: self.terrain[middle_row, middle_col-1],
+					2: self.terrain[middle_row-1, middle_col-1],
+					3: self.terrain[middle_row-1, middle_col],
+					4: self.terrain[middle_row-1, middle_col+1],
+					5: self.terrain[middle_row, middle_col+1],
+					6: self.terrain[middle_row+1, middle_col+1],
+					7: self.terrain[middle_row+1, middle_col],
+					8: self.terrain[middle_row+1, middle_col-1],
+				}
+		return options[direction]
+
 
 	def findPeak(self):
 		if getTerrainSize<=4:
@@ -13,7 +27,7 @@ class 2DPeak(object):
 		middle_col = (self.terrain.shape[1])/2
 
 		if middle_row==0 and middle_col==0:
-			neighbors = {a: 0 for a in [5,6,7]}
+			neighbors = {a: getElement(middle_row, middle_column, a) for a in [5,6,7]}
 
 		elif middle_row==0 and middle_col==len(self.terrain[0][0])-1:
 			1,8,7
