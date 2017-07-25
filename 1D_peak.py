b98e2c12ec2f9eaf9ca2a70cc16cc1dc96fda01e
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-24 20:14:44 -0400

    retsructured the class

--- 1D_peak.py
+++ 1D_peak.py
@@ -4,22 +4,24 @@ s = [1,2,3,4,4,5,6,3,2,1,3]
 class OneDPeak(object):
 	def __init__(self, terrain):
 		self.terrain = terrain
-		
-def find_peak(a, start, end):
-	middle = (start+end)/2
-	if a[middle-1]<=a[middle] and a[middle+1]<=a[middle]:
-		print a[middle]
-		return
-	if start==end:
-		print a[start]
-		return
-	if end==start+1:
-		print max(a[start], a[end])
-		return
-	else:
-		if a[middle]>a[middle-1] and a[middle]<=a[middle+1]:
-			find_peak(a, middle+1, end)
-		else:
-			find_peak(a, start, middle-1)
 
-find_peak(s,0,len(s)-1)
+	def find_peak(self, start, end):
+		middle = (start+end)/2
+		if self.terrain[middle-1]<=self.terrain[middle] and self.terrain[middle+1]<=self.terrain[middle]:
+			print self.terrain[middle]
+			return
+		if start==end:
+			print self.terrain[start]
+			return
+		if end==start+1:
+			print max(self.terrain[start], self.terrain[end])
+			return
+		else:
+			if self.terrain[middle]>self.terrain[middle-1] and self.terrain[middle]<=self.terrain[middle+1]:
+				self.find_peak(middle+1, end)
+			else:
+				self.find_peak(start, middle-1)
+
+
+terrain = OneDPeak(s)
+terrain.find_peak(0,len(s)-1)
