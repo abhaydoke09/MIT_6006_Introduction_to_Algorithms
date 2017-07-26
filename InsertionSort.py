c467f3dd320b43d79ed1737719e0d78cd5c280c0
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-25 23:45:50 -0400

    added class for sorting

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,14 +1,22 @@
-def insertionSort(l):
-	for i in range(1,len(l)):
-		key = l[i]
-		for searchIndex in range(i-1,-1,-1):
-			if l[searchIndex]<key:
-				break
-			else:
-				temp = l[searchIndex]
-				l[searchIndex] = key
-				l[searchIndex+1] = temp
-	return l
+class Sort(object):
+	def __init__(self, l):
+		self.l = l
 
-s = [5,4,2,6,1,3]
-print insertionSort(s)
+	def insertionSort(self):
+		for i in range(1,len(self.l)):
+			key = self.l[i]
+			for searchIndex in range(i-1,-1,-1):
+				if self.l[searchIndex]<key:
+					break
+				else:
+					temp = self.l[searchIndex]
+					self.l[searchIndex] = key
+					self.l[searchIndex+1] = temp
+
+	def displayList(self):
+		print self.l
+
+s = [10,-5,5,6,1,3]
+sort = Sort(s)
+sort.insertionSort()
+sort.displayList()
