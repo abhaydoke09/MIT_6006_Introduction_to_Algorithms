1db51c8522f71437ad27b5df7f4d2fce49df3b1b
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-25 23:52:13 -0400

    measuring time required for the sorting

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,8 +1,17 @@
+'''
+Class Sort has different sort methods.
+Call and compare the time required to complete the sorting using different methods
+'''
+
+import time
+import random
+
 class Sort(object):
 	def __init__(self, l):
 		self.l = l
 
 	def insertionSort(self):
+		start = time.clock()
 		for i in range(1,len(self.l)):
 			key = self.l[i]
 			for searchIndex in range(i-1,-1,-1):
@@ -12,11 +21,14 @@ class Sort(object):
 					temp = self.l[searchIndex]
 					self.l[searchIndex] = key
 					self.l[searchIndex+1] = temp
+		end = time.clock()
+		print 'Time in seconds required to complete the sorting ==>', end-start
 
 	def displayList(self):
 		print self.l
 
-s = [10,-5,5,6,1,3]
+s = [random.randint(-100,100) for i in xrange(1000)]
+
 sort = Sort(s)
 sort.insertionSort()
 sort.displayList()
